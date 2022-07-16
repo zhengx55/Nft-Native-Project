bff49d1a00be3fa692f9d42a5653a63268824298
zhengx55 <williamzhxin@gmail.com>
2022-07-16 16:53:03 +0800

    project setup

---
+++ screens/Home.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Text, SafeAreaView, FlatList, View } from "react-native";
+const Home = () => {
+  return <Text>Home</Text>;
+};
+
+export default Home;
